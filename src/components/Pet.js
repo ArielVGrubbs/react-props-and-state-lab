08552f5ec5fd7b1eed09a420da8375d6a4477fba
ariel_grubbs <arielvictor1234@gmail.com>
2020-12-11 14:28:19 -0600

    Done.

--- src/components/Pet.js
+++ src/components/Pet.js
@@ -1,25 +1,43 @@
 import React from 'react'
 
 class Pet extends React.Component {
+  handleButtons = (event) => {
+    const adoptButton = event.target
+    const adoptedButton = event.target.parentElement.firstElementChild
+
+    adoptButton.classList.remove('primary')
+    adoptButton.classList.add('disabled')
+    adoptedButton.classList.remove('disabled')
+    adoptedButton.classList.add('primary')
+  }
+
+  handleGender = (gender) => {
+    if (gender === 'male') {
+      return '♂ '
+    } else {
+      return '♀ '
+    }
+  }
+
   render() {
     return (
       <div className="card">
         <div className="content">
-          <a className="header">
-            {/*'♀' OR '♂' */}
-            PET NAME
-          </a>
+          <p className="header">
+            {this.handleGender(this.props.petInst.gender)}
+            {this.props.petInst.name}
+          </p>
           <div className="meta">
-            <span className="date">PET TYPE</span>
+            <span className="date">{this.props.petInst.type}</span>
           </div>
           <div className="description">
-            <p>Age: PET AGE</p>
-            <p>Weight: PET WEIGHT</p>
+            <p>Age: {this.props.petInst.age}</p>
+            <p>Weight: {this.props.petInst.weight}</p>
           </div>
         </div>
         <div className="extra content">
-          <button className="ui disabled button">Already adopted</button>
-          <button className="ui primary button">Adopt pet</button>
+          <button className="ui disabled button" >Already adopted</button>
+          <button className="ui primary button" onClick={event => this.handleButtons(event)}>Adopt pet</button>
         </div>
       </div>
     )
--- src/components/PetBrowser.js
+++ src/components/PetBrowser.js
@@ -3,8 +3,18 @@ import React from 'react'
 import Pet from './Pet'
 
 class PetBrowser extends React.Component {
+  helperMethod = () => {
+    return (
+      this.props.petArray.map(pet => <Pet petInst={pet} key={pet.id}/>)
+     ) 
+  }
+
   render() {
-    return <div className="ui cards">PET COMPONENT SHOULD GO HERE</div>
+    return (
+      <div className="ui cards">
+        { this.helperMethod() }
+      </div>
+    )
   }
 }
 
